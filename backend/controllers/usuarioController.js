9a7fe8b5a2d8c6969e4e9c37789dd11ea0653475
Daniel Mateu <danielmateu86@gmail.com>
2022-12-30 16:09:46 +0100

    Autenticación usuario mail y confirmación de mail

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -26,6 +26,23 @@ const registrar = async (req, res, next) => {
 
 const autenticar = async (req, res, next) => {
 
+    const {email, password} = req.body;
+
+    //Comprobar si el usuario existe
+    const usuario = await Usuario.findOne({email})
+    if(!usuario){
+        const error = new Error('El usuario no existe');
+        return res.status(404).json({msg: error.message})
+    }
+    
+    //Comprobar si el usuario está confirmado
+    if(!usuario.confirmado){
+        const error = new Error('Debes validar tu email');
+        return res.status(403).json({msg: error.message})
+    }
+
+    //Comprobar el pssword
+    
 }
 
 
