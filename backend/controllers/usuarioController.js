ef597085fc6d31614e3b4e65ea610e166571ef3f
Daniel Mateu <danielmateu86@gmail.com>
2022-12-30 14:17:49 +0100

    Prevención de usuarios duplicados

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -5,6 +5,15 @@ import Usuario from "../models/Usuario.js"
 
 const registrar = async (req, res, next) => {
 
+    //Evitar registros duplicados
+    const {email} = req.body;
+    const existeUsuario = await Usuario.findOne({email});
+
+    if(existeUsuario){
+        const error = new Error('Usuario ya registrado')
+        return res.status(400).json({msg: error.message})
+    }
+
     try {
         const usuario = new Usuario(req.body);
         const usuarioAlmacenado = await usuario.save();
@@ -13,8 +22,6 @@ const registrar = async (req, res, next) => {
     } catch (error) {
         console.log(error)
     }
-
-
 }
 
 
