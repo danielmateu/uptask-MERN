cecaca8461dcf1ec4c372827e494c9a30e1f5c61
Daniel Mateu <danielmateu86@gmail.com>
2023-01-01 21:01:40 +0100

    Obtener proyectos de usuario autenticado

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -1,7 +1,9 @@
 import { Proyecto } from "../models/Proyecto.js"
 
 const obtenerProyectos = async (req, res) => {
+    const proyectos = await Proyecto.find().where('creador').equals(req.usuario);
 
+    res.json(proyectos);
 }
 
 const nuevoProyecto = async (req, res) => {
