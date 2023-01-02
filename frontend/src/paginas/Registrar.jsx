d4ab6d8e107c14176c0cbd1a6920bf088d08902b
Daniel Mateu <danielmateu86@gmail.com>
2023-01-02 18:05:17 +0100

    Añadiendo state al formulario de registro

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 
 export const Registrar = () => {
+
+
+    const [nombre, setNombre] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [repetirPassword, setRepetirPassword] = useState('');
 
     return (
         <>
@@ -11,19 +18,46 @@ export const Registrar = () => {
             <form action="" className="my-10 bg-white rounded-lg shadow-md p-10 ">
                 <div className="my-5">
                     <label htmlFor="nombre" className="text-gray-600 block">Nombre</label>
-                    <input id='nombre' type="text" placeholder="Nombre de usuario" className="w-full mt-3 p-3 rounded-xl" />
+                    <input
+                        id='nombre'
+                        type="text"
+                        placeholder="Nombre de usuario" className="w-full mt-3 p-3 rounded-xl" 
+                        value={nombre}
+                        onChange={e => setNombre(e.target.value)}
+                    />
                 </div>
                 <div className="my-5">
                     <label htmlFor="email" className="text-gray-600 block">Email</label>
-                    <input id='email' type="email" placeholder="Email de registro" className="w-full mt-3 p-3 rounded-xl" />
+                    <input
+                        id='email'
+                        type="email"
+                        placeholder="Email de registro" 
+                        className="w-full mt-3 p-3 rounded-xl" 
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
                 </div>
                 <div className="my-5">
                     <label htmlFor="password" className="text-gray-600 block">Password</label>
-                    <input id='password' type="password" placeholder="Password de registro" className="w-full mt-3 p-3 rounded-xl" />
+                    <input
+                        id='password'
+                        type="password"
+                        placeholder="Password de registro" 
+                        className="w-full mt-3 p-3 rounded-xl"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}    
+                    />
                 </div>
                 <div className="my-5">
                     <label htmlFor="password2" className="text-gray-600 block">Repite tu Password</label>
-                    <input id='password2' type="password" placeholder="Repite tu Password" className="w-full mt-3 p-3 rounded-xl" />
+                    <input
+                        id='password2'
+                        type="password"
+                        placeholder="Repite tu Password" 
+                        className="w-full mt-3 p-3 rounded-xl" 
+                        value={repetirPassword}
+                        onChange={e => setRepetirPassword(e.target.value)}
+                    />
                 </div>
 
                 <input type="submit" value='Crear cuenta' className="bg-sky-400 p-4 rounded-xl shadow-md hover:shadow-none transition-all font-semibold text-white hover:cursor-pointer hover:text-gray-500 w-full mb-5"  />
