74b11f50d31c64d8beaecddb4255221328953b9f
Daniel Mateu <danielmateu86@gmail.com>
2023-01-08 19:59:48 +0100

    Validando token para resetear password

--- frontend/src/paginas/NuevoPasword.jsx
+++ frontend/src/paginas/NuevoPasword.jsx
@@ -1,38 +1,73 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Link, useParams } from "react-router-dom"
+import { Alerta } from "../components/Alerta"
 
 
 
 export const NuevoPasword = () => {
 
+    const [tokenValido, setTokenValido] = useState(false)
+    const [alerta, setAlerta] = useState('')
+
+    const params = useParams();
+    const { token } = params;
+
+    useEffect(() => {
+        const comprobarToken = async () => {
+            try {
+                //TODO mover hacia un cliente axios
+                await axios(`http://localhost:4000/api/usuarios/olvide-password/${token}`);
+                setTokenValido(true);
+
+            } catch (error) {
+                setAlerta({
+                    msg:error.response.data.msg,
+                    error: true
+                })
+            }
+        }
+        comprobarToken();
+    }, [])
+
+    const {msg} = alerta;
 
     return (
         <>
-            <h1 className="text-sky-400 text-6xl font-semibold">Reestablece tu password y no pierdas el acceso a tus {' '}<span className="text-slate-400">proyectos</span>
+            <h1 className="text-sky-400 text-6xl font-semibold my-10">Reestablece tu password y no pierdas el acceso a tus {' '}<span className="text-slate-400">proyectos</span>
             </h1>
 
-            <form action="" className="my-10 bg-white rounded-lg shadow-md p-10 ">
-                
-                <div className="my-5">
-                    <label htmlFor="password" className="text-gray-600 block">Nuevo password</label>
-                    <input id='password' type="password" placeholder="Escribe tu password" className="w-full mt-3 p-3 rounded-xl" />
-                </div>
-                
+            {
+                msg && <Alerta alerta={alerta}/>
+            }
 
-                <input type="submit" value='Nuevo password' className="bg-sky-400 p-4 rounded-xl shadow-md hover:shadow-none transition-all font-semibold text-white hover:cursor-pointer hover:text-gray-500 w-full mb-5"  />
-            </form>
+            {
+                tokenValido && (
+                    <form action="" className="my-10 bg-white rounded-lg shadow-md p-10 ">
 
-            <nav className="lg:flex lg:justify-between">
+                        <div className="my-5">
+                            <label htmlFor="password" className="text-gray-600 block">Nuevo password</label>
+                            <input id='password' type="password" placeholder="Escribe tu password" className="w-full mt-3 p-3 rounded-xl" />
+                        </div>
+
+
+                        <input type="submit" value='Nuevo password' className="bg-sky-400 p-4 rounded-xl shadow-md hover:shadow-none transition-all font-semibold text-white hover:cursor-pointer hover:text-gray-500 w-full mb-5" />
+                    </form>
+                )
+            }
+
+            <nav className="lg:flex lg:justify-between my-10">
                 <Link
                     className="block text-center my-4 text-slate-500 hover:text-slate-600"
                     to='/'
                 >
-                ¿Ya estás registrado? Inicia sesión
+                    ¿Ya estás registrado? Inicia sesión
                 </Link>
                 <Link
                     className="block text-center my-4 text-slate-500 hover:text-slate-600"
                     to='/olvide-password'
                 >
-                Olvidé mi Password
+                    Olvidé mi Password
                 </Link>
 
             </nav>
