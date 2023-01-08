ff1c644d009ef180055b51e16617ab59f07fbad3
Daniel Mateu <danielmateu86@gmail.com>
2023-01-08 21:31:59 +0100

    Creando context de autenticación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthProvider'
 import { AuthLayout } from './layouts/AuthLayout'
-import {ConfirmarCuenta, Login, NuevoPasword, OlvidePassword, Registrar} from './paginas'
+import { ConfirmarCuenta, Login, NuevoPasword, OlvidePassword, Registrar } from './paginas'
 
 
 // console.log(import.meta.env.VITE_BACKEND_URL)
@@ -11,21 +12,17 @@ function App() {
   return (
 
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path='registrar' element={<Registrar />} />
-          <Route path='olvide-password/' element={<OlvidePassword />} />
-          <Route path='olvide-password/:token' element={<NuevoPasword />} />
-          <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
-        </Route>
-      </Routes>
-
-      {/* <Routes>
-        <Route path="/">
-
-        </Route>
-      </Routes> */}
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path='registrar' element={<Registrar />} />
+            <Route path='olvide-password/' element={<OlvidePassword />} />
+            <Route path='olvide-password/:token' element={<NuevoPasword />} />
+            <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,25 @@
+
+import { createContext, useState, useEffect } from 'react'
+
+import {} from 'react-router-dom'
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+
+    return (
+        <AuthContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export {
+    AuthProvider
+}
+
+export default AuthContext
